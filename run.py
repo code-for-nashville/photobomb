5f911f66c5bfc105cc49d6000adc64fdc9b56a5e
shannon <smcoll@gmail.com>
2016-09-12 15:44:59 -0500

    log callback request

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+import logging
 
-@app.route("/")
+from flask import Flask, request
+
+
+app = Flask(__name__)
+logger = logging.getLogger(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
+    from_number = request.values.get('From', None)
+
+    logger.debug('Request: '.format(request))
+    logger.debug('From: '.format(from_number))
+    return "Hello {}".format(from_number or 'world!')
 
 if __name__ == "__main__":
     app.run(debug=True)
