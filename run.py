54815054ee02774b8d573afe2db2ac01944e6f93
shannon <smcoll@gmail.com>
2016-09-13 09:21:16 -0500

    allow hard-coded mimtype:extension mappings, and limit From number to last 10 (drop country code)

--- run.py
+++ run.py
@@ -7,9 +7,13 @@ from flask import Flask, request
 app = Flask(__name__)
 dbx = dropbox.Dropbox(os.environ.get('DROPBOX_TOKEN'))
 
+CONTENTTYPE_CONFIG = {
+    'image/jpeg': 'jpeg'
+}
+
 
 @app.route("/", methods=['GET', 'POST'])
-def hello():
+def handle():
     """ Handle a Twilio SMS/MMS callback request: https://www.twilio.com/docs/api/twiml/sms/twilio_request
 
     Request keys: MessageSid, ToZip, SmsSid, NumMedia, NumSegments, ToCountry, ApiVersion, Body, FromCountry, To,
@@ -24,8 +28,8 @@ def hello():
             media_url = request.values.get('MediaUrl{}'.format(i))
             content_type = request.values.get('MediaContentType{}'.format(i))
             message_id = media_url.rsplit('/', 1)[-1]
-            extension = mimetypes.guess_extension(content_type)
-            upload_path = u'/{}/{}{}'.format(phone_number.strip('+'), message_id, extension)
+            extension = CONTENTTYPE_CONFIG.get(content_type, mimetypes.guess_extension(content_type))
+            upload_path = u'/{}/{}{}'.format(phone_number[-10:], message_id, extension)
             dbx.files_save_url(upload_path, media_url)
             uploads.append({'upload_path': upload_path, 'content_type': content_type})
         # TODO: append metadata of the SMS/MMS to (if it exists - else create) a hidden file
